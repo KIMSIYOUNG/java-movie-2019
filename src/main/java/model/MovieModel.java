14e1e66d1ff2b4f50961f3a0e184538e43966b20
KIMSIYOUNG <tldud2404@gmail.com>
2019-12-18 18:45:56 +0900

    feat : 예약할 인원을 입력받고, 추가적 예약을 할지 결제로 넘어갈지 결정하는 기능

--- src/main/java/model/MovieModel.java
+++ src/main/java/model/MovieModel.java
@@ -4,29 +4,36 @@ import java.io.IOException;
 import java.util.List;
 
 import domain.Movie;
+import domain.User;
+import utils.MoreOrStop;
 import view.InputView;
 import view.OutputView;
 
 public class MovieModel {
     private static List<Movie> movies;
     private static InputView inputView;
+    private static User user;
 
     public MovieModel(List<Movie> movies) {
+        user = new User();
         this.movies = movies;
+        inputView = new InputView(movies);
     }
 
     public void reserve() throws IOException {
-        inputView = new InputView(movies);
-        inputMovieId();
+        do {
+            inputAndAddToUser();
+        } while (!inputView.moreOrStop());
+    }
+
+    private void inputAndAddToUser() throws IOException {
         int movieId = inputView.inputMovieId();
         OutputView.printSelectedMovie(movies, movieId);
         int movieTime = inputMovieTime(movieId);
-
-    }
-
-    private void inputMovieId() {
-
-
+        int howMany = inputView.inputHowManyPeople(movieId, movieTime);
+        for(int i =0; i< howMany; i++){
+            user.addMovie(getMovieById(movieId),movieTime);
+        }
     }
 
     private int inputMovieTime(int movieId) throws IOException {
@@ -42,5 +49,19 @@ public class MovieModel {
             movieTime = InputView.inputMovieTime(movie);
         }
         return movieTime;
+    }
+
+    private Movie getMovieById(int movieId) {
+        for(Movie movie : movies){
+            movieIdCompareToUserInput(movieId, movie);
+        }
+        return null;
+    }
+
+    private Movie movieIdCompareToUserInput(int movieId, Movie movie) {
+        if(movie.getId()==movieId){
+            return movie;
+        }
+        return null;
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import domain.Movie;
@@ -16,6 +15,11 @@ public class InputView {
     private static final int ZERO = 0;
     private static final String INPUT_MOVIE_TIME = "예매할 시간표를 선택하세요. (위에서부터 1번)";
     private static final String ERROR_NO_SEATS = "그 시간은 이미 마감되었습니다. 다른 시간대를 선택해주세요.";
+    private static final String INPUT_HOW_MANY_PEOPLE = "예약할 인원을 입력하세요.";
+    private static final String ERROR_OVER_NUMBER = "예약인원을 초과하였습니다. ";
+    private static final String MORE_OR_STOP = "예약을 종료하고 결제를 진행하려면 1번, 예약을 추가하려면 2번을 눌러주세요.";
+    private static final int STOP = 1;
+    private static final int MORE = 2;
 
     private List<Movie> movieList;
 
@@ -60,4 +64,59 @@ public class InputView {
         System.out.println(ERROR_NO_SEATS);
         return inputMovieTime(movie);
     }
+
+    public int inputHowManyPeople(int movieId, int movieTime) {
+        System.out.println(INPUT_HOW_MANY_PEOPLE);
+        try {
+            int userInput = Integer.parseInt(BR.readLine().trim());
+            return checkCanReserveThisNumbers(userInput,movieId,movieTime);
+        }catch (NumberFormatException | IOException e) {
+            System.out.println(ERROR_FORMAT);
+            return inputHowManyPeople(movieId,movieTime);
+        }
+    }
+
+    private int checkCanReserveThisNumbers(int howMany, int movieId, int movieTime) {
+        if(getMovieById(movieId).getPlaySchedules()
+                .get(movieTime).canReserve(howMany)){
+            return howMany;
+        }
+        System.out.println(ERROR_OVER_NUMBER);
+        return inputHowManyPeople(movieId,movieTime);
+    }
+
+    private Movie getMovieById(int movieId) {
+        for(Movie movie : movieList){
+            movieIdCompareToUserInput(movieId, movie);
+        }
+        return null;
+    }
+
+    private Movie movieIdCompareToUserInput(int movieId, Movie movie) {
+        if(movie.getId()==movieId){
+            return movie;
+        }
+        return null;
+    }
+
+    public boolean moreOrStop() throws IOException {
+        System.out.println(MORE_OR_STOP);
+        try {
+            int userInput = Integer.parseInt(BR.readLine().trim());
+            return checkUserInput(userInput);
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR_FORMAT);
+            return moreOrStop();
+        }
+    }
+
+    private boolean checkUserInput(int userInput) throws IOException {
+        if(userInput==MORE)
+            return true;
+        if(userInput==STOP)
+            return false;
+        System.out.println(ERROR_FORMAT);
+        return moreOrStop();
+    }
+
 }
